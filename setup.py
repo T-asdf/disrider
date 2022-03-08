61da748ab70e632baf02dde59a82bb4c533bcee9
seojun <seojune5383@naver.com>
2022-03-08 10:12:17 +0900

    init

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(name='disrider',
     description='더 쉽게, 더 빠르게. 카트라이더 전적 검색.',
     author='ㅁㄴㅇㄹ#5332',
     author_email='seojune5383@naver.com',
-    url='https://blog.naver.com/seojune5383',
+    url='https://github.com/T-asdf/disrider.git',
     license='MIT',
     py_modules=['disrider'],
     python_requires='>=3',
     install_requires=['discord', 'requests'],
-    packages=['disrider']
+    packages=find_packages(exclude=['metadata'])
 )
